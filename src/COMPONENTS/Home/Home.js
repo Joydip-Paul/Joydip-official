81ff709b4287ee84ed6c622575cf8d3312af45f3
Joydip Paul <joydippaul72@gmail.com>
2021-08-10 04:02:14 +0600

    hero section is customized

--- src/COMPONENTS/Home/Home.js
+++ src/COMPONENTS/Home/Home.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import './Home.css';
 // import Hero from '../../img/dp.jpg';
-import { AiFillHtml5 } from 'react-icons/ai';
-import { DiCss3, DiJavascript, DiReact } from 'react-icons/di';
-import { BsBootstrapFill } from 'react-icons/bs';
-import { SiTailwindcss } from 'react-icons/si';
+import { AiFillGithub, AiFillFacebook, AiFillInstagram, AiFillLinkedin, AiFillTwitterSquare, AiFillSkype } from 'react-icons/ai';
 
 const Home = () => {
     return (
@@ -12,31 +9,28 @@ const Home = () => {
             <div className="container">
                 <div className="row justify-content-center">
                     <div className="hero-content text-center">
-                        {/* <h1 data-text="Joydip_Paul" className="hero-heading">
-                            Joydip_Paul
-                        </h1> */}
                         <h1 className="hero-heading">
                             Joydip Paul
                         </h1>
                         <small>Web Developer</small>
-                        <div className="p-3 skill-icon">
+                        <div className="py-3 skill-icon">
                             <div className="single-skill-icon">
-                                <AiFillHtml5 />
+                                <AiFillGithub />
                             </div>
                             <div className="single-skill-icon">
-                                <DiCss3 />
+                                <AiFillFacebook />
                             </div>
                             <div className="single-skill-icon">
-                                <DiJavascript />
+                                <AiFillInstagram />
                             </div>
                             <div className="single-skill-icon">
-                                <DiReact />
+                                <AiFillLinkedin />
                             </div>
                             <div className="single-skill-icon">
-                                <BsBootstrapFill />
+                                <AiFillTwitterSquare />
                             </div>
                             <div className="single-skill-icon">
-                                <SiTailwindcss />
+                                <AiFillSkype />
                             </div>
                         </div>
                         <button className="theme-btn">Download CV</button>
